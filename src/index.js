47340a46f27652f6650e535c43a972924ec7207c
Till Prochaska <1512805+tillprochaska@users.noreply.github.com>
2025-01-06 18:42:38 +0100

    Remove unused remark plugin to rewrite the base of absolute URLs.
    All of our docs sites are now served at the root path, so this isn’t required anymore. Also, if this requirement comes up again in the future, the better solution would probably be to use relative URLs in documentation contents.

--- src/index.js
+++ src/index.js
@@ -6,17 +6,15 @@ import defaultClasses from './plugins/defaultClasses.js';
 import injectComponent from './plugins/injectComponent.js';
 import wrapContents from './plugins/wrapContents.js';
 import customCodeComponent from './plugins/customCodeComponent.js';
-import fixAbsoluteLinks from './plugins/fixAbsoluteLinks.js';
 
 const srcRoot = path.dirname(url.fileURLToPath(import.meta.url));
 const componentsEntry = path.join(srcRoot, 'components/index.js');
 
-function mdx(astroConfig, remarkPlugins = []) {
+function mdx(remarkPlugins = []) {
   return mdxBase({
     extendMarkdownConfig: true,
     remarkPlugins: [
       defaultClasses(),
-      fixAbsoluteLinks(astroConfig.base),
       customCodeComponent(),
       wrapContents('RichContent'),
       injectComponent('RichContent', componentsEntry, 'named'),
@@ -31,26 +29,14 @@ function mdx(astroConfig, remarkPlugins = []) {
       injectComponent('Step', componentsEntry, 'named'),
       injectComponent('Mermaid', componentsEntry, 'named'),
       injectComponent('Markdown', componentsEntry, 'named'),
+      ...remarkPlugins,
     ],
   });
 }
 
-export default ({ remarkPlugins = [] } = {}) => {
-  return {
-    name: 'astro-theme-docs',
-    hooks: {
-      'astro:config:setup': async (hookParams) => {
-        // This is a hacky workaround in order to be able to access the
-        // Astro site base URL when initializing remark plugins. This may
-        // break when the MDX integration changes (e.g. when it starts
-        // relying on additional hooks).
-        const mdxIntegration = mdx(hookParams.config, remarkPlugins);
-        const setupHook = mdxIntegration.hooks['astro:config:setup'];
-        await setupHook(hookParams);
-      },
-    },
-  };
-};
+export default ({ remarkPlugins }) => [
+  mdx(remarkPlugins),
+];
 
 export * from './utils/git.js';
 export { defaultClasses, injectComponent, wrapContents, customCodeComponent };
